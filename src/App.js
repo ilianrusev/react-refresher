23597a9f48aa9d2306272698432ba30e8aac649f
Ilian Rusev <ilianrusev@gmail.com>
2023-01-17 19:09:35 +0200

    wrapper component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavouritesPage from "./pages/Favourites";
 import TodosPage from "./pages/Todos";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 export default function App() {
   return (
-    <>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favourites" element={<FavouritesPage />} />
         <Route path="/todos" element={<TodosPage />} />
       </Routes>
-    </>
+    </Layout>
   );
 }
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,20 +1,23 @@
+import Card from "../ui/Card";
 import styles from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
   return (
-    <li className={styles.item}>
-      <div className={styles.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={styles.description}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={styles.actions}>
-        <button>to favourites</button>
-      </div>
-    </li>
+    <Card>
+      <li className={styles.item}>
+        <div className={styles.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={styles.description}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={styles.actions}>
+          <button>to favourites</button>
+        </div>
+      </li>
+    </Card>
   );
 }
 
