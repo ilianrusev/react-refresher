c971c7a43338fc5b6160463cd284a2a77ff0172a
Ilian Rusev <ilianrusev@gmail.com>
2023-01-17 16:16:35 +0200

    make a todo component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import Todo from "./components/Todo";
+
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+  );
 }
