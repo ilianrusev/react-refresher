3b4935688dc013b62cf6d82f56acebf84862236a
Ilian Rusev <ilianrusev@gmail.com>
2023-01-17 20:03:45 +0200

    functionality to add new meetups with firebase realtime db

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 export default function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      process.env.REACT_APP_FIREBASE_DB_MEETUPS, // add your db here
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
